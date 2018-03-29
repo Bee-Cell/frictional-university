080fb404cfe8a071833dcbb52e818a95194f8d6c
Bishal Bhattarai <bhattaraibishal50@gmail.com>
2018-03-29 15:08:29 +0545

    fine control on menus with in html using if statement

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -13,40 +13,41 @@
           <div class="site-footer__col-two">
             <h3 class="headline headline--small">Explore</h3>
 
-            <?php
+           <!-- <?php
               //footer location one
-              wp_nav_menu(array(
-                'theme_location' => 'footerLocationOne'
-              ));
-            ?>
+              // wp_nav_menu(array(
+              //   'theme_location' => 'footerLocationOne'
+              // ));
+            ?> --> 
 
-            <!-- <nav class="nav-list">
+            <nav class="nav-list">
               <ul>
-                <li><a href="<?php  echo site_url("/about-us"); ?>">About Us</a></li>
+                <li><a href="<?php  //echo site_url("/about-us"); ?>">About Us</a></li>
                 <li><a href="#">Programs</a></li>
                 <li><a href="#">Events</a></li>
                 <li><a href="#">Campuses</a></li>
               </ul>
-            </nav> -->
+            </nav> 
           </div>
 
           <div class="site-footer__col-three">
             <h3 class="headline headline--small">Learn</h3>
 
-            <?php
+            <!-- <?php
               //footer location 2
 
-            wp_nav_menu(array(
-                'theme_location' => 'footerLocationTwo'
-              ));
-            ?>
-            <!-- <nav class="nav-list">
+            // wp_nav_menu(array(
+            //     'theme_location' => 'footerLocationTwo'
+            //   ));
+            ?> -->
+
+            <nav class="nav-list">
               <ul>
                 <li><a href="#">Legal</a></li>
                 <li><a href="<?php  echo site_url("/privacy-policy"); ?>">Privacy</a></li>
                 <li><a href="#">Careers</a></li>
               </ul>
-            </nav> -->
+            </nav> 
           </div>
         </div>
 
