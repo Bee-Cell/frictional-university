db743a56e9ca6e9a379c6708f0c6c2e1670916e1
Bishal Bhattarai <bhattaraibishal50@gmail.com>
2018-03-29 14:03:30 +0545

    complet navigation between related page and non realated pages

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -36,14 +36,45 @@
     ?>
 
     
-    
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div> -->
+
+    <?php
+
+      //return the current page child page ids
+      $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+
+      //only if the page have children or relational pages
+      if ($theParent or $testArray) {
+    ?>
+       <!--  menu link for applicatable children pages -->
+        <div class="page-links">
+          <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent); ?></a></h2>
+          <ul class="min-list">
+            <?php
+
+              if($theParent){
+                $findChildrenOF = $theParent;
+              }
+              else{
+                //it will b zero so use the current id
+                $findChildrenOF = get_the_ID();
+              }
+
+
+
+              //for listing pages 
+            wp_list_pages(array(
+              'title_li' => NULL, //no need page ttile"
+              'child_of' => $findChildrenOF,
+              'sort_column' => 'menu_order'    //custom ordering the list
+              )); //provide all pages 
+            ?>
+          </ul>
+        </div>
+    <?php
+      }
+    ?>
 
     <div class="generic-content">
       <?php the_content(); ?>
