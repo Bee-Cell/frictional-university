b4ae177e963e6f43fd799db1c4529b2ffdae0647
Bishal Bhattarai <bhattaraibishal50@gmail.com>
2018-03-29 11:45:18 +0545

    complete transforing the main index.html page

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -3,13 +3,16 @@
 //to load java scrits css files
 function university_files(){
 
+	//loading js file
+	wp_enqueue_script('main-university-js', get_theme_file_uri('/assets/js/scripts-bundled.js'), NULL, microtime() , true); //null for depndencies, version 1, load at beging or at last true, false
+
 	//font-asesome
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
 	//custom fonts
 	wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 	//main styles
-	wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+	wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
 }
 
 //for css loadng scripts css
